0fdcfe66eb9f07d208954f1b3a344903b7389a62
Jakob Runge <jaru@spreadshirt.net>
2021-03-07 16:26:43 +0100

    Remove old files

--- component---src-pages-404-js-c5c7dc1c307d429f14ae.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x9427c64ab85d],{272:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var n=u(2),a=l(n),d=function(){return a.default.createElement("div",null,a.default.createElement("h1",null,"NOT FOUND"),a.default.createElement("p",null,"You just hit a route that doesn't exist… the sadness."))};t.default=d,e.exports=t.default}});
-//# sourceMappingURL=component---src-pages-404-js-c5c7dc1c307d429f14ae.js.map
